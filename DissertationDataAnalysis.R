e0d1bb8e053ca19c2ce11e45da13fdb66d9a30f5
offTrailAud <taud14@yahoo.com>
2017-04-03 11:40:52 -0700

    Audrey's Work

--- DissertationDataAnalysis.R
+++ DissertationDataAnalysis.R
@@ -127,4 +127,139 @@ print(sm6) #nothing significant
 
 write.csv(sm3$coef, file = "Model3Results.csv")
 
+############################################################################################
 
+#creating plot to determine interaction effect on m3:
+
+ggplot(df, aes(y = skills, x = total.b)) + #This calls the data and sets the x and y axes
+  geom_point(aes(size = total.s)) + #This adds points to the plot
+  ylab("Skills") + #These change the labels of the x and y axes (by default they get the name of your variable which may not be very useful to an outside reader)
+  xlab("Total.b") + #replace this with something intelligible
+  scale_size_continuous("Total.s") +#this changes the name of the size legend, make it make sense
+  theme_bw() #This adds a simplified theme 
+
+#Try again with color instead of size
+ggplot(df, aes(y = skills, x = total.b)) + #This calls the data and sets the x and y axes
+  geom_point(aes(color = total.s), size = 2) + #This adds points to the plot with the color mapped to total.s
+  ylab("Skills") + #These change the labels of the x and y axes (by default they get the name of your variable which may not be very useful to an outside reader)
+  xlab("Total.b") + #replace this with something intelligible
+  scale_color_continuous("Total.s") +#this changes the name of the size legend, make it make sense
+  theme_bw() #This adds a simplified theme 
+
+#Both of these plots indicate to me that the interaction is slightly attenuating
+
+#lets try reversing the predictors
+ggplot(df, aes(y = skills, x = total.s)) + #This calls the data and sets the x and y axes
+  geom_point(aes(size = total.b)) + #This adds points to the plot
+  ylab("Skills") + #These change the labels of the x and y axes (by default they get the name of your variable which may not be very useful to an outside reader)
+  xlab("Total.s") + #replace this with something intelligible
+  scale_size_continuous("Total.b") +#this changes the name of the size legend, make it make sense
+  theme_bw() #This adds a simplified theme 
+
+ggplot(df, aes(y = skills, x = total.s)) + #This calls the data and sets the x and y axes
+  geom_point(aes(color = total.b), size = 2) + #This adds points to the plot
+  ylab("Skills") + #These change the labels of the x and y axes (by default they get the name of your variable which may not be very useful to an outside reader)
+  xlab("Total.s") + #replace this with something intelligible
+  scale_color_continuous("Total.b") +#this changes the name of the size legend, make it make sense
+  theme_bw() #This adds a simplified theme 
+
+
+#Sometimes is it helpful to have the response as the third dimension
+ggplot(df, aes(y = total.b, x = total.s)) + 
+  geom_point(aes(size = skills)) +
+  theme_bw()
+
+
+ggplot(df, aes(y = total.b, x = total.s)) + 
+  geom_point(aes(color = skills), size = 2) +
+  theme_bw()
+
+#Hmmm, there is an outlying value that may be driving the attenuation.  
+#I am suspicious that the interaction might dissappear or change signs if this data point is removed.
+
+
+#refit the model with only skills greater than 5
+#First find the outlier
+ind <- which(df$skills < 5) #observation 33
+
+#Refit the model after removing the outlier
+m3New <- lm(skills ~ total.s + total.b + total.s:total.b, data = df[-ind, ])
+
+summary(m3New)
+
+#As I suspected the removal of this point made the interaction non-significant
+#I would suggest re-running the other models without this observation since it 
+#appears to be an outlier.  Or indeed verify that it should be included.
+
+
+#here is a prediciton function so you can see how it is done
+predictM3 <- function(total.s, total.b){
+  skills <- -28.72 + 0.340955*total.s + 1.032546*total.b + -0.007551*total.s*total.b
+  return(skills)
+}
+
+#find the range of values
+summary(df[, c("total.s", "total.b", "skills")])
+
+
+predictM3(total.s = 58, total.b = 22)#with both low
+
+#Looking at the outlier: Observation #33
+df[, 33]
+
+############################################################################################
+#Interaction effect with exercise
+
+df$total.e <- df$e195+df$e196+df$e197
+
+# Factored (academic engagement) model with interaction
+m4e <- lm(emot ~ total.s + total.e + total.s:total.e, data = df)
+#Create summary of the model for evaluation
+sm4e <- summary(m4e)
+# View the summary
+print(sm4e) #nothing significant
+
+m3e <- lm(skills ~ total.s + total.e + total.s:total.e, data = df)
+#Create summary of the model for evaluation
+sm3e <- summary(m3e)
+# View the summary
+print(sm3e) #nothing significant
+
+m5e <- lm(part ~ total.s + total.e + total.s:total.e, data = df)
+#Create summary of the model for evaluation
+sm5e <- summary(m5e)
+# View the summary
+print(sm5e) #nothing significant
+
+m6e <- lm(perf ~ total.s + total.e + total.s:total.e, data = df)
+#Create summary of the model for evaluation
+sm6e <- summary(m6e)
+# View the summary
+print(sm6e) #nothing significant
+
+
+#Simple plot of the relationship between s and e
+ggplot(df, aes(x = total.s, y = total.e)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
+
+# correlation of s and e
+cse <- cor(df$total.s, df$total.e, use = "pairwise.complete.obs")
+
+
+#Simple plot of the relationship between s and m
+ggplot(df, aes(x = total.s, y = total.m)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
+
+# correlation of s and m
+csm <- cor(df$total.s, df$total.m, use = "pairwise.complete.obs")
+
+
+#Simple plot of the relationship between e and m
+ggplot(df, aes(x = total.e, y = total.m)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
+
+# correlation of e and m
+cem <- cor(df$total.e, df$total.m, use = "pairwise.complete.obs")
